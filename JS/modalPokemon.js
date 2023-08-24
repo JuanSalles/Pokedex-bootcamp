737ca82dd26a434302e146026453c9e006c79faa
Juan Salles <juanprazeres@gmail.com>
2023-08-24 12:48:10 -0300

    Id do pokemon adicionado ao modal

--- JS/modalPokemon.js
+++ JS/modalPokemon.js
@@ -4,14 +4,16 @@ const imagePokemonInsideModal = document.getElementById('imagem-do-pokemon');
 const boxModal = document.getElementById('box-modal');
 const titlePokemon = document.getElementById('nome-do-pokemon');
 const typeInsideModal = document.getElementById('pokemonType-modal');
+const elementoPokemonId = document.getElementById('modal__pokemon-id')
 
 export default function modalPokemon(pokemon){
     detalhesPokemon(pokemon);
-    typeInsideModal.textContent = "";
     boxModal.setAttribute(`data-type`, pokemon.mainType);
     imagePokemonInsideModal.setAttribute('src', pokemon.picture);
     titlePokemon.textContent = pokemon.name;
+    elementoPokemonId.textContent = "#"+pokemon.id;
     typeInsideModal.innerHTML = criaElementoDeTipos(pokemon.types);
+    
 }
 
 function criaElementoDeTipos(listaDeTipos){
